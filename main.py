fa45e1775c9bc78c5005613a3f6f223aac60d727
Ken He <hetingxu88@gmail.com>
2024-01-15 15:15:31 +1030

    Implemented the bigram model

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+#hyperparameters
+
+block_size = 8
+batch_size = 32
+learning_rate = 1e-3
+max_train_iteration = 20000
+#------
 
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 chars = sorted(list(set(text)))
+print(chars)
 vocab_size = len(chars)
 
 stoi = { ch:i for i,ch in enumerate(chars) }
@@ -17,11 +28,73 @@ n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
-block_size = 8
 
 x = train_data[:block_size]
 y = train_data[1:block_size+1]
 for t in range(block_size):
     context = x[:t+1]
     target = y[t]
-    print(f"when input is {context} the target: {target}")
+
+def get_batch(data_type):
+    data = train_data if data_type == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size, ))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i + 1:i+block_size + 1] for i in ix])
+    return x, y
+
+xb, yb = get_batch("train")
+
+for b in range(batch_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target = yb[b, t]
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets = None):
+        logits = self.token_embedding_table(idx) #(batch, block, channel(64))
+        if targets == None:
+            loss = None
+        else:
+            batch, block, channel = logits.shape
+            logits = logits.view(batch*block, channel)
+            targets = targets.view(batch*block)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1) # (Batch, 1)
+            idx = torch.cat((idx, idx_next), dim=1) # (Batch, Block + 1)
+
+        return idx
+    
+model = BigramLanguageModel(vocab_size=vocab_size)
+logits, loss = model(xb, yb)
+print("Before any training: ")
+generated_text = model.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0]
+print(decode(generated_text.tolist()))
+
+optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate)
+
+for step in range(max_train_iteration):
+    #get training data in batch
+    xb, yb = get_batch("train")
+
+    #evaluate loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print("After some training: ")
+print(loss.item())
+generated_text = model.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0]
+print(decode(generated_text.tolist()))
