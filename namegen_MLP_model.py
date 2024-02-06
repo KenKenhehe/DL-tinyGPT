fe284ac4af7bc8bf065437be62ea4cdb948bbae0
Ken He <hetingxu88@gmail.com>
2024-02-06 16:50:29 +1030

    Add inference code

--- namegen_MLP_model.py
+++ namegen_MLP_model.py
@@ -101,3 +101,23 @@ logits = h1_output @ W2 + b2
 loss = F.cross_entropy(logits, y_dev)
 
 print(f"test loss: {loss.item()}")
+
+def sample(num: int):
+    g = torch.Generator()
+    for _ in range(num):
+        out = []
+        context = [0] * block_size
+        while True:
+            embedding = lookup_table[torch.tensor([context])]
+            h1_output = torch.tanh(embedding.view(1, -1) @ W1 + b1)
+            logits = h1_output @ W2 + b2
+            probs = F.softmax(logits, dim=1)
+            #sample from distribution
+            index = torch.multinomial(probs, num_samples=1, generator=g).item()
+            context = context[1:] + [index]
+            out.append(index)
+            if index == 0:
+                break
+        print("".join(itos[i] for i in out))
+        
+sample(20) 
